d3e829cb61907ff4a974c2132f6f14b0abd37450
potatobob <83433649+potatobob@users.noreply.github.com>
2023-12-31 12:23:36 -0500

    e

---
+++ CountdownClock/Pods/Target Support Files/Pods-CountdownClockTests/Pods-CountdownClockTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CountdownClockTests : NSObject
+@end
+@implementation PodsDummy_Pods_CountdownClockTests
+@end
---
+++ CountdownClock/Pods/Target Support Files/Pods-CountdownWidgetIntentExt/Pods-CountdownWidgetIntentExt-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CountdownWidgetIntentExt : NSObject
+@end
+@implementation PodsDummy_Pods_CountdownWidgetIntentExt
+@end
